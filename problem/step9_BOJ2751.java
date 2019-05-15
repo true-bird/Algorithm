989819999b8ad5929793c284ede954e7a09a176c
sparrow <isc9683@naver.com>
2019-05-15 17:52:39 +0900

    수 정렬하기 2 ( 병합 정렬 )

--- problem/step9_BOJ2751.java
+++ problem/step9_BOJ2751.java
@@ -4,68 +4,64 @@ import java.io.*;
  * 문제 : 수 정렬하기
  * 입력 : 숫자의 개수 N ( 1<=N<= 1,000,000 )
  * 출력 : 오름차 순 결과
- * 힙 소트
- * - 힙 구조 상 루트에 가장 큰 값이 존재함을 이용
+ * 머지 소트
+ * - 쪼갠 후 정렬, 합병
+ * - 링크드 리스트에 효율적
  */
 
 public class step9_BOJ2751 {
 
     static int N;
     static int[] num;
+    static int[] sorted;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
         num = new int[N];
-
+        sorted = new int[N];
         for(int i=0;i<N;i++) {
             num[i] = Integer.parseInt(br.readLine());
         }
-        heapSort(num);
-        for(int i=0;i<N;i++) {
-            bw.write(num[i]+"\n");
+
+        new step9_BOJ2751().mergeSort(num,0,N-1);
+
+        for(int n : num) {
+            bw.write(n+"\n");
         }
         bw.flush();
     }
 
-    static public void heapSort(int[] array) {
-        // 삽입
-        int n = array.length;
-
-        for(int i=n/2-1; i>=0; i--) { // 리프 노트를 제외한 마지막 노드부터
-            heapify(array,n,i);
-        }
-        for(int i=n-1; i>0; i--) { // 루트 노드를 맨 뒤로 보내고 맨 뒤의 배열을 제외하고 힙 구현
-            swap(array,0,i);
-            heapify(array,i,0);
+    public void mergeSort(int[] array, int left, int right) { // 쪼갠 후 정렬
+        if(left<right) {
+            int mid = (left + right) /2;
+            // 쪼개기
+            mergeSort(array,left,mid);
+            mergeSort(array,mid+1,right);
+            // 정렬
+            merge(array, left, mid, right);
         }
     }
 
-    static public void heapify(int[] array, int n, int i) {
-        int p = i;
-        int l = i * 2 + 1;
-        int r = i * 2 + 2;
+    public void merge(int[] array, int left, int mid, int right) {
 
-        if ( l<n && array[p]<array[l] ) {
-            p = l;
+        int i = left, j = mid+1, k = left;
+
+        while (i <=mid && j <= right) {
+            if (array[i] <= array[j]) sorted[k++] = array[i++];
+            else sorted[k++] = array[j++];
+        }
+        // 남아 있는 수 채우기
+        while (i <= mid) {
+            sorted[k++] = array[i++];
+        }
+        while (j <= right) {
+            sorted[k++] = array[j++];
         }
 
-        if ( r<n && array[p]<array[r] ) {
-            p = r;
+        for(int n=left;n<=right;n++) { // 재복사
+            array[n] = sorted[n];
         }
-
-        if (i != p) {
-            swap(array, p, i);
-            heapify(array, n, p); // 재귀
-        }
-        // 셋 중 가장 큰 값이 위로
     }
-
-    static public void swap(int[] array, int a, int b) {
-        int tmp = array[a];
-        array[a] = array[b];
-        array[b] = tmp;
-    }
-
 }
